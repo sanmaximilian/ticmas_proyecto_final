5042c937f7ef81d66350fc69efa579d76ab44e3f
Santiago Maximiliano <sanmaximilianb@gmail.com>
2023-09-26 13:41:52 -0300

    agregando un poco de javascript!

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 console.log("Listo JS")
+console.log("Hacer que el javascript oculte el form cuando se de enviar y aparezca un mensaje")
+
+function agregGreet(){
+
+    var demo1 = document.createElement("h1");
+    var placing = document.createTextNode("The Heading One is Creaated");
+    demo1.appendChild(placing);
+}
+
+function ocultaForm(){
+    let mensaje= "Gracias por enviar sus datos, en breve sera contactado"
+    
+    document.getElementById("contactoR").style.display="none"
+    // document.getElementById("contacT").
+    
+    // document.createElement("h1")
+    
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode("");
+    newDiv.appendChild(newContent); //añade texto al div creado.
+  
+    // añade el elemento creado y su contenido al DOM
+    var currentDiv = document.getElementById("contacT");
+    document.body.insertBefore(newDiv, currentDiv);
+    
+
+
+
+    console.log("Aca esto deberia de ocultar el form poner algun mensaje de gracias y usar el nombre del que lo lleno con un gracias")
+}
